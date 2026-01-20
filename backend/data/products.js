afbb4ed91e76a38c0372298777ffc6e775e6d6ac
Pratik patil <pratikmanjre806@gmail.com>
2026-01-20 12:52:39 +0530

    Success: MongoDB connected and Seeder script populated data

---
+++ backend/data/products.js
@@ -0,0 +1,38 @@
+// backend/data/products.js
+const products = [
+  {
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/airpods.jpg',
+    description: 'Bluetooth technology lets you connect it with compatible devices wirelessly',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'iPhone 13 Pro 256GB Memory',
+    image: '/images/phone.jpg',
+    description: 'Introducing the iPhone 13 Pro. A transformative triple-camera system.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Sony Playstation 5',
+    image: '/images/playstation.jpg',
+    description: 'The ultimate home entertainment center starts with PlayStation.',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 5,
+    rating: 5,
+    numReviews: 12,
+  }
+];
+
+export default products;
